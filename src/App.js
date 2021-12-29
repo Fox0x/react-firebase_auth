f56e95d69938cc9ec32a292b133311755ba2f5cf
Kirill <foxxx2game@gmail.com>
2021-12-29 18:04:27 +0300

    init new proj from part of another

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import WelcomeForm from "./components/WelcomeForm/WelcomeForm";
+import LoginForm from "./components/LoginForm/LoginForm";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={<AuthPage wrappedComponent={<WelcomeForm />} />}
+			/>
+			<Route
+				path="/auth/login"
+				element={<AuthPage wrappedComponent={<LoginForm />} />}
+			/>
+			<Route
+				path="/auth/register"
+				element={<AuthPage wrappedComponent={<RegisterForm />} />}
+			/>
+		</Routes>
+	);
 }
 
 export default App;
---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,46 @@
+import React, { useMemo } from "react";
+import css from "./AuthPage.module.css";
+import SvgBackground from "../components/UI/SvgBackground/SvgBackground";
+import { Link } from "react-router-dom";
+
+export default function AuthPage({ wrappedComponent }) {
+	const color = useMemo(() => {
+		switch (wrappedComponent.type.name) {
+			case "WelcomeForm":
+				return "#DB2379";
+			case "LoginForm":
+				return "#DC2430";
+			case "RegisterForm":
+				return "#F4B400";
+			default:
+				return "#DB2379";
+		}
+	}, [wrappedComponent.type.name]);
+
+	return (
+		<div className={css.auth__wrapper}>
+			<div className={css.auth__content}>
+				<div className={css.auth__leftSide}>
+					<span className={css.auth__title}>
+						Очень воодушевляющий слоган...
+					</span>
+					<img
+						src="/img/logo.svg"
+						alt="logo"
+						className={css.auth__logo}
+					/>
+				</div>
+
+				{wrappedComponent}
+			</div>
+			<SvgBackground color={color} />
+			<div className={css.auth__skip}>
+				<span>
+					This is just a test sample, so you can{" "}
+					<Link style={{color: "#dbdfea"}} to="/dash">skip the authentication step</Link>
+					{" "}and go to the main menu.
+				</span>
+			</div>
+		</div>
+	);
+}
